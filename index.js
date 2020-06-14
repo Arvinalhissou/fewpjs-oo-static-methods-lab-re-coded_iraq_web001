949aa64e6afa351ec78b68903b0dbf3bb44c0d32
Arvin  Alhissou <arvin.alhissou@gmail.com>
2020-06-14 23:30:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,7 +6,19 @@ class Formatter {
    return str.replace(/[^A-Za-z0-9-']+/g, '');
 }
   static titleize(str){
-    return str.
+    let finalStr = []
+    let nonCap = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from']
+    let newStr = str.split(' ')
+    finalStr.push(Formatter.capitalize(newStr[0]))
+    for(let i = 1; i < newStr.length; i++){
+      if(nonCap.includes(newStr[i]) === false) {
+        finalStr.push(Formatter.capitalize(newStr[i]))
+      }else {
+        finalStr.push(newStr[i])
+      }
+    }
+    return finalStr.join(' ')
+  }
   }
   
 }
